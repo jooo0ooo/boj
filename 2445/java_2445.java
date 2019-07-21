b80ab76b02f6d01183bf034ec9d222fe188dc5ee
Jiwoo Jung <wldn3634@naver.com>
2019-07-21 18:03:49 +0900

    folder setting...

---
+++ 2445/java_2445.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class java_2445 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		
+		int size = s.nextInt();
+		for(int i = 0; i < size; i++) {
+			
+			for(int k = 0; k < size - i - 1; k++) {
+				System.out.print(" ");
+			}
+			
+			for(int j = 0; j < i * 2 + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
+	}
+
+}
