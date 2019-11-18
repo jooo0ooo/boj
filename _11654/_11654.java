c57e390257d598cf51f189b97488d3ea9a250c64
Jiwoo Jung <wldn3634@naver.com>
2019-11-18 13:54:13 +0900

    boj 11654

---
+++ _11654/_11654.java
@@ -0,0 +1,18 @@
+package _11654;
+
+import java.util.Scanner;
+
+public class _11654 {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		char c = s.next().charAt(0);
+		
+		System.out.println((int)c);
+		
+		s.close();
+		
+	}
+
+}
