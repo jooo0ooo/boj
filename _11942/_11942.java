6408e5abb688fa07c84d2db86ec1d3e3ef8b800c
Jiwoo Jung <wldn3634@naver.com>
2019-11-18 15:47:37 +0900

    boj 11942

---
+++ _11942/_11942.java
@@ -0,0 +1,21 @@
+package _11942;
+
+public class _11942 {
+
+	public static void main(String[] args) {
+
+		System.out.println("고려대학교");
+	
+	}
+	
+	// 
+	/**
+	 * 
+	 * 제출 시 언어 Text로 설정한 후 
+	 * 
+	 * 고려대학교
+	 *   
+	 * 그대로 붙여넣으세용
+	 * */
+
+}
