d8bee6559c30b4a3454c5e8b871bb507b029f323
Jiwoo Jung <wldn3634@naver.com>
2019-11-18 14:08:20 +0900

    boj 10171

---
+++ _10171/_10171.java
@@ -0,0 +1,27 @@
+package _10171;
+
+public class _10171 {
+
+	public static void main(String[] args) {
+
+		System.out.println("\\    /\\\r\n" + 
+				" )  ( ')\r\n" + 
+				"(  /  )\r\n" + 
+				" \\(__)|");
+	
+	}
+	
+	// 
+	/**
+	 * 
+	 * 제출 시 언어 Text로 설정한 후 
+	 * 
+	 * \    /\
+	 *  )  ( ')
+	 * (  /  )
+	 *  \(__)|
+	 *  
+	 *  그대로 붙여넣으세용
+	 * */
+
+}
