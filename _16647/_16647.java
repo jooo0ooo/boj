e36e800429c9fc5284262ccff6ddf111d207e980
Jiwoo Jung <wldn3634@naver.com>
2020-01-01 02:37:04 +0900

    boj 16647

---
+++ _16647/_16647.java
@@ -0,0 +1,21 @@
+package _16647;
+
+public class _16647 {
+
+	public static void main(String[] args) {
+
+		System.out.println("진심을 담은 생일 축하 편지를 쓰면 통과되는 것 같아요ㅎㅎ");
+	
+	}
+	
+	// 
+	/**
+	 * 
+	 * 제출 시 언어 Text로 설정한 후 
+	 * 
+	 * 진심을 담은 생일 축하 편지를 쓰면 통과되는 것 같습니다ㅎㅎ
+	 *   
+	 * 그대로 붙여넣으세용
+	 * */
+
+}
