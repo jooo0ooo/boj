22d7de849540c98a09ce865eedce8631be976c9e
Jiwoo Jung <wldn3634@naver.com>
2020-01-01 02:18:48 +0900

    boj 10170

---
+++ _10170/_10170.java
@@ -0,0 +1,46 @@
+package _10170;
+
+public class _10170 {
+
+	public static void main(String[] args) {
+
+		System.out.println("NFC West       W   L  T\n" + 
+				"-----------------------\n" + 
+				"Seattle        13  3  0\n" + 
+				"San Francisco  12  4  0\n" + 
+				"Arizona        10  6  0\n" + 
+				"St. Louis      7   9  0\n" + 
+				"\n" + 
+				"NFC North      W   L  T\n" + 
+				"-----------------------\n" + 
+				"Green Bay      8   7  1\n" + 
+				"Chicago        8   8  0\n" + 
+				"Detroit        7   9  0\n" + 
+				"Minnesota      5  10  1");
+	
+	}
+	
+	// 
+	/**
+	 * 
+	 * 제출 시 언어 Text로 설정한 후 
+	 * 
+	 * 
+	 * NFC West       W   L  T
+	 * -----------------------
+	 * Seattle        13  3  0
+	 * San Francisco  12  4  0
+	 * Arizona        10  6  0
+	 * St. Louis      7   9  0
+	 * NFC North      W   L  T
+	 * -----------------------
+	 * Green Bay      8   7  1
+	 * Chicago        8   8  0
+	 * Detroit        7   9  0
+	 * Minnesota      5  10  1
+	 * 
+	 *   
+	 *  그대로 붙여넣으세용
+	 * */
+
+}
