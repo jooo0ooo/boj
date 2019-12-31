4f7e625bb0848ed9290ccb41df7666ec6ca0af01
Jiwoo Jung <wldn3634@naver.com>
2020-01-01 01:25:42 +0900

    boj 15962

---
+++ _15962/_15962.java
@@ -0,0 +1,21 @@
+package _15962;
+
+public class _15962 {
+
+	public static void main(String[] args) {
+
+		System.out.println("파이팅!!");
+	
+	}
+	
+	// 
+	/**
+	 * 
+	 * 제출 시 언어 Text로 설정한 후 
+	 * 
+	 * 파이팅!!
+	 *   
+	 * 그대로 붙여넣으세용
+	 * */
+
+}
