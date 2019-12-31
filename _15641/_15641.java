2120cf2b57aa41b11b36166a20a0c2dfc7a26c7c
Jiwoo Jung <wldn3634@naver.com>
2020-01-01 02:29:20 +0900

    boj 15641

---
+++ _15641/_15641.java
@@ -0,0 +1,22 @@
+package _15641;
+
+public class _15641 {
+
+	public static void main(String[] args) {
+
+		System.out.println("53");
+	
+	}
+	
+	// 
+	/**
+	 * 
+	 * 제출 시 언어 Text로 설정한 후 
+	 * 
+	 * 1 ~ 100 사이 숫자를 넣으면 된다(정답값은 랜덤)
+	 * Binary Search 방식으로 하니씩 넣어보며 진행하여 맞춤
+	 *   
+	 * 그대로 붙여넣으세용
+	 * */
+
+}
