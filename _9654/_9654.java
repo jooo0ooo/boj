27c1e33dd819fda7f5ca993b21bdca2764196dd0
Jiwoo Jung <wldn3634@naver.com>
2020-01-01 01:57:05 +0900

    boj 9654

---
+++ _9654/_9654.java
@@ -0,0 +1,33 @@
+package _9654;
+
+public class _9654 {
+
+	public static void main(String[] args) {
+
+		System.out.println("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n" + 
+				"N2 Bomber      Heavy Fighter  Limited    21        \n" + 
+				"J-Type 327     Light Combat   Unlimited  1         \n" + 
+				"NX Cruiser     Medium Fighter Limited    18        \n" + 
+				"N1 Starfighter Medium Fighter Unlimited  25        \n" + 
+				"Royal Cruiser  Light Combat   Limited    4         ");
+	
+	}
+	
+	// 
+	/**
+	 * 
+	 * 제출 시 언어 Text로 설정한 후 
+	 * 
+	 * 
+	 * SHIP NAME      CLASS          DEPLOYMENT IN SERVICE
+	 * N2 Bomber      Heavy Fighter  Limited    21        
+	 * J-Type 327     Light Combat   Unlimited  1         
+	 * NX Cruiser     Medium Fighter Limited    18        
+	 * N1 Starfighter Medium Fighter Unlimited  25        
+	 * Royal Cruiser  Light Combat   Limited    4         
+	 * 
+	 *   
+	 *  그대로 붙여넣으세용
+	 * */
+
+}
