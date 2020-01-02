1b794914a40752a9b87735444d60228eec9f8268
Jiwoo Jung <wldn3634@naver.com>
2020-01-02 12:43:52 +0900

    boj 15890

---
+++ _15890/_15890.java
@@ -0,0 +1,21 @@
+package _15890;
+
+public class _15890 {
+
+	public static void main(String[] args) {
+
+		System.out.println("20180729");
+	
+	}
+	
+	// 
+	/**
+	 * 
+	 * 제출 시 언어 Text로 설정한 후 
+	 * 
+	 * 20180729
+	 *   
+	 * 그대로 붙여넣으세용
+	 * */
+
+}
