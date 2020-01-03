1d6ba7efe8afcef830bce994d5b19731232820e0
Jiwoo Jung <wldn3634@naver.com>
2020-01-03 16:09:40 +0900

    boj 15733

---
+++ _15733/_15733.java
@@ -0,0 +1,21 @@
+package _15733;
+
+public class _15733 {
+
+	public static void main(String[] args) {
+
+		System.out.println("I'm Sexy");
+	
+	}
+	
+	// 
+	/**
+	 * 
+	 * 제출 시 언어 Text로 설정한 후 
+	 * 
+	 * I'm Sexy
+	 *   
+	 * 그대로 붙여넣으세용
+	 * */
+
+}
