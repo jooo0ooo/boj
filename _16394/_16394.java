8ec70e45d50ae5ed8199542aa99fbd8c26095790
Jiwoo Jung <wldn3634@naver.com>
2020-01-09 22:51:28 +0900

    boj 16394

---
+++ _16394/_16394.java
@@ -0,0 +1,18 @@
+package _16394;
+
+import java.util.Scanner;
+
+public class _16394 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		
+		int year = s.nextInt();
+		
+		System.out.println(year - 1946);
+		
+		s.close();
+	}
+
+}
