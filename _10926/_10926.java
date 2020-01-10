69ebe82eb938a930aa4d3c9b7ed7befe514db788
Jiwoo Jung <wldn3634@naver.com>
2020-01-10 15:06:37 +0900

    boj 10926

---
+++ _10926/_10926.java
@@ -0,0 +1,17 @@
+package _10926;
+
+import java.util.Scanner;
+
+public class _10926 {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		
+		String id = s.nextLine();
+				
+		System.out.println(id+"??!");
+	
+	}
+
+}
