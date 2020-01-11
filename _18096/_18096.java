79e9703f1099bfffbd9cb41d88f324d91856a2d2
Jiwoo Jung <wldn3634@naver.com>
2020-01-11 22:21:56 +0900

    boj 18096

---
+++ _18096/_18096.java
@@ -0,0 +1,21 @@
+package _18096;
+
+public class _18096 {
+
+	public static void main(String[] args) {
+
+		System.out.println("1");
+	
+	}
+	
+	// 
+	/**
+	 * 
+	 * 제출 시 언어 Text로 설정한 후 
+	 * 
+	 * 1
+	 *   
+	 * 그대로 붙여넣으세용
+	 * */
+
+}
