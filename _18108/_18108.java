f8cfa7825ffc10fc9e3c5ad3045b8f1a35a9fbcb
Jiwoo Jung <wldn3634@naver.com>
2020-01-11 22:38:29 +0900

    boj 18108

---
+++ _18108/_18108.java
@@ -0,0 +1,18 @@
+package _18108;
+
+import java.util.Scanner;
+
+public class _18108 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		
+		int year = s.nextInt();
+		
+		System.out.println(year - 543);
+		
+		s.close();
+	}
+
+}
