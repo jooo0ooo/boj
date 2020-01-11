e998a5779cfbba6bf1b232aaed12b71da45f21f6
Jiwoo Jung <wldn3634@naver.com>
2020-01-11 23:09:53 +0900

    boj 16170

--- _16170/_16170.java
+++ _16170/_16170.java
@@ -1,12 +1,28 @@
 package _16170;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class _16170 {
 
 	public static void main(String[] args) {
 
-		long time = System.currentTimeMillis();
-		System.out.println(time);
+		ZoneId utc = ZoneId.of("UTC");
 
+        ZonedDateTime utcTime = ZonedDateTime.of(LocalDateTime.now(utc), utc);
+        
+        DateTimeFormatter timeFormat = null;
+        
+        timeFormat = DateTimeFormatter.ofPattern("yyyy");
+        System.out.println(utcTime.format(timeFormat));
+        
+        timeFormat = DateTimeFormatter.ofPattern("MM");
+        System.out.println(utcTime.format(timeFormat));
+        
+        timeFormat = DateTimeFormatter.ofPattern("dd");
+        System.out.println(utcTime.format(timeFormat));
 	
 	}
 
