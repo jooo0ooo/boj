e74d50eee9f43ab25b7ac3ba7cb60279a0dd99bc
Jiwoo Jung <wldn3634@naver.com>
2020-01-11 22:14:53 +0900

    boj 14918

---
+++ _14918/_14918.java
@@ -0,0 +1,19 @@
+package _14918;
+
+import java.util.Scanner;
+
+public class _14918 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		System.out.println(a + b);
+		
+		s.close();
+	}
+
+}
