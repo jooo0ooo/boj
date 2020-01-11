4c4ecfe605c8f923e54aff34be51dcd16a563dc6
Jiwoo Jung <wldn3634@naver.com>
2020-01-11 22:41:57 +0900

    boj 15439

---
+++ _15439/_15439.java
@@ -0,0 +1,18 @@
+package _15439;
+
+import java.util.Scanner;
+
+public class _15439 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		
+		int n = s.nextInt();
+		
+		System.out.println(n * n - n);
+		
+		s.close();
+	}
+
+}
