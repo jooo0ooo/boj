4b7941eecb7d00719c6a1084947df73a2674a195
Jiwoo Jung <wldn3634@naver.com>
2020-01-11 22:17:47 +0900

    boj 17295

---
+++ _17295/_17295.java
@@ -0,0 +1,21 @@
+package _17295;
+
+public class _17295 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Avengers: Endgame");
+	
+	}
+	
+	// 
+	/**
+	 * 
+	 * 제출 시 언어 Text로 설정한 후 
+	 * 
+	 * Avengers: Endgame
+	 *   
+	 * 그대로 붙여넣으세용
+	 * */
+
+}
