de72b920f7c89e67ee48062d2e6c481f48ebf92d
Jiwoo Jung <wldn3634@naver.com>
2020-01-13 12:47:11 +0900

    boj 16430

---
+++ _16430/_16430.java
@@ -0,0 +1,18 @@
+package _16430;
+
+import java.util.Scanner;
+
+public class _16430 {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		System.out.println(b-a + " " + b);
+		
+	}
+
+}
