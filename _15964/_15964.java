604738ff8bda3d86c1c1d5777149f99ab8cb1a0a
Jiwoo Jung <wldn3634@naver.com>
2020-01-13 12:43:25 +0900

    boj 15964
    partial success
    Requires modification

---
+++ _15964/_15964.java
@@ -0,0 +1,18 @@
+package _15964;
+
+import java.util.Scanner;
+
+public class _15964 {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		System.out.println((a+b) * (a-b));
+		
+	}
+
+}
