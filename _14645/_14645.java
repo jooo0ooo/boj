3e083b013c5ceaba5daae7ba70c7c239626f79dc
Jiwoo Jung <wldn3634@naver.com>
2020-01-19 00:07:56 +0900

    boj 14645

---
+++ _14645/_14645.java
@@ -0,0 +1,21 @@
+package _14645;
+
+public class _14645 {
+
+	public static void main(String[] args) {
+
+		System.out.println("비와이");
+	
+	}
+	
+	// 
+	/**
+	 * 
+	 * 제출 시 언어 Text로 설정한 후 
+	 * 
+	 * 비와이
+	 *   
+	 * 그대로 붙여넣으세용
+	 * */
+
+}
