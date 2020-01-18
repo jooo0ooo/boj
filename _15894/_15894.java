d9165f64ae86383158f80832e2d000cd2779bf11
Jiwoo Jung <wldn3634@naver.com>
2020-01-19 00:22:15 +0900

    boj 15894

---
+++ _15894/_15894.java
@@ -0,0 +1,17 @@
+package _15894;
+
+import java.util.Scanner;
+
+public class _15894 {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		
+		long n = s.nextLong();
+		
+		System.out.println(4 * n);
+		
+	}
+
+}
