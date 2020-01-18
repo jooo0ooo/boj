8e6cbf1ff998d193b048127e005971e5306ac98d
Jiwoo Jung <wldn3634@naver.com>
2020-01-19 00:24:38 +0900

    boj 5522

---
+++ _5522/_5522.java
@@ -0,0 +1,21 @@
+package _5522;
+
+import java.util.Scanner;
+
+public class _5522 {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		
+		int a1 = s.nextInt();
+		int a2 = s.nextInt();
+		int a3 = s.nextInt();
+		int a4 = s.nextInt();
+		int a5 = s.nextInt();
+		
+		System.out.println(a1 + a2 + a3 + a4 + a5);
+		
+	}
+
+}
