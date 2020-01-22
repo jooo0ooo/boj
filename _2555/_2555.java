f841d263276ba5057fa862a43c761632cef51f28
Jiwoo Jung <wldn3634@naver.com>
2020-01-22 12:49:21 +0900

    boj 2555

---
+++ _2555/_2555.java
@@ -0,0 +1,21 @@
+package _2555;
+
+public class _2555 {
+
+	public static void main(String[] args) {
+
+		System.out.println("10/14");
+	
+	}
+	
+	// 
+	/**
+	 * 
+	 * 제출 시 언어 Text로 설정한 후 
+	 * 
+	 * 10/14
+	 *   
+	 * 그대로 붙여넣으세용
+	 * */
+
+}
