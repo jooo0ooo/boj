f449208846a0bbdff99cbe4dfa07755fb381d678
Jiwoo Jung <wldn3634@naver.com>
2020-01-25 22:57:58 +0900

    boj 2743

---
+++ _2743/_2743.java
@@ -0,0 +1,17 @@
+package _2743;
+
+import java.util.Scanner;
+
+public class _2743 {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println(s.nextLine().length());
+		
+		s.close();
+		
+	}
+
+}
