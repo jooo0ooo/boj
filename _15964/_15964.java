1336e0bdfa943d6bf603e76688bc6d48e99da2e3
Jiwoo Jung <wldn3634@naver.com>
2020-02-17 21:40:14 +0900

    boj 15964(fixed)

--- _15964/_15964.java
+++ _15964/_15964.java
@@ -1,17 +1,30 @@
 package _15964;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
 
 public class _15964 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		Scanner s = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int a = s.nextInt();
-		int b = s.nextInt();
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		System.out.println((a+b) * (a-b));
+		String a = st.nextToken();
+		String b = st.nextToken();
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		bw.write(new BigInteger(a).add(new BigInteger(b)).multiply(new BigInteger(a).subtract(new BigInteger(b))) + "");
+		
+		bw.flush();
+		bw.close();
 		
 	}
 
