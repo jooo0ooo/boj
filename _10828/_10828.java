6043d2573fa9449672614700df5689cc71843f2f
joo <pingrae@me.com>
2020-12-08 01:24:18 +0900

    boj 10828

---
+++ _10828/_10828.java
@@ -0,0 +1,64 @@
+package _10828;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class _10828 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int[] stack = new int[10000];
+		int commandSize = Integer.parseInt(input.readLine()), stackSize = 0;
+		
+		for (int i = 0; i < commandSize; i++) {
+			StringTokenizer st = new StringTokenizer(input.readLine());
+			
+			String command = st.nextToken();
+			
+			if (command.startsWith("push")) {
+				stack[stackSize] = Integer.parseInt(st.nextToken());
+				stackSize++;
+			} else if (command.startsWith("pop")) {
+				if (stackSize > 0) {
+					bw.write(stack[stackSize - 1]+"");
+				} else {
+					bw.write(-1 + "");
+				}
+				bw.newLine();
+				if (stackSize > 0) {
+					stackSize--;
+				}
+			} else if (command.startsWith("size")) {
+				bw.write(stackSize + "");
+				bw.newLine();
+			} else if (command.startsWith("empty")) {
+				if (stackSize > 0) {
+					bw.write(0 + "");
+				} else {
+					bw.write(1 + "");
+				}
+				bw.newLine();
+			} else if (command.startsWith("top")) {
+				if (stackSize > 0) {
+					bw.write(stack[stackSize - 1] + "");
+				} else {
+					bw.write(-1 + "");
+				}
+				bw.newLine();
+			}
+		}
+		
+		bw.flush();
+		bw.close();
+		
+	}
+
+}
