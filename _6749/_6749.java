2f376b2e3f8d42487da94f29b6e3631bae5ce27a
joo <pingrae@me.com>
2020-12-09 23:08:07 +0900

    boj 6749

---
+++ _6749/_6749.java
@@ -0,0 +1,27 @@
+package _6749;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _6749 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int third = Integer.parseInt(br.readLine()), second = Integer.parseInt(br.readLine());
+		
+		bw.write(second + second - third + "");
+		
+		bw.flush();
+		bw.close();
+		
+	}
+}
