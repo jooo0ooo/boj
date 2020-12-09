a4d55cc6d59cf54e96e92f8398cf441cf0f06d14
joo <pingrae@me.com>
2020-12-09 23:02:50 +0900

    boj 2338

---
+++ _2338/_2338.java
@@ -0,0 +1,28 @@
+package _2338;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _2338 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		BigInteger first = new BigInteger(br.readLine()), 
+				second = new BigInteger(br.readLine());
+		
+		bw.write(first.add(second) + "\n" + first.subtract(second) + "\n" + first.multiply(second));
+		
+		bw.flush();
+		bw.close();
+		
+	}
+}
