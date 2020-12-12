0a70572caec5aa622c9c961b86050688fcedba2e
joo <pingrae@me.com>
2020-12-13 00:29:30 +0900

    boj 1212

---
+++ _1212/_1212.java
@@ -0,0 +1,27 @@
+package _1212;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+
+public class _1212 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		BigInteger a = new BigInteger(br.readLine(), 8);
+		
+		bw.write(a.toString(2));
+		
+		
+		
+		bw.flush();
+		bw.close();
+		
+	}
+}
