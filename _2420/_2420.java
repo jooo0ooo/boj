138b4fa1b09e1aa46e97c1d6f01ba5573d2f56f9
joo <pingrae@me.com>
2020-12-16 23:10:39 +0900

    boj 2420

---
+++ _2420/_2420.java
@@ -0,0 +1,25 @@
+package _2420;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class _2420 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		bw.write((new BigInteger(st.nextToken())).subtract(new BigInteger(st.nextToken())).abs() + "");
+		
+		bw.flush();
+		bw.close();
+	}
+
+}
