32dbd0a1924af956f784c9ee96e105349fdbb10e
joo <pingrae@me.com>
2020-12-20 22:50:05 +0900

    boj 14935

---
+++ _14935/_14935.java
@@ -0,0 +1,24 @@
+package _14935;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class _14935 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String input = br.readLine();
+		
+		bw.write("FA");
+		
+		bw.flush();
+		bw.close();
+		
+	}
+}
