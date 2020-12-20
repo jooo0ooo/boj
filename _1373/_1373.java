51abf94f6091912a7e454b68eac8fb3ff231de71
Jiwoo Jung <pingrae@me.com>
2020-12-21 00:16:10 +0900

    boj 1373

---
+++ _1373/_1373.java
@@ -0,0 +1,25 @@
+package _1373;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+
+public class _1373 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		BigInteger a = new BigInteger(br.readLine(), 2);
+		
+		bw.write(a.toString(8));
+		
+		bw.flush();
+		bw.close();
+		
+	}
+}
