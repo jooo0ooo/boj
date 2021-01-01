074715bc4a027cb3d4fdade76f66694cf6c62a06
joo <pingrae@me.com>
2021-01-01 20:06:54 +0900

    boj 11722

---
+++ _11722/_11722.java
@@ -0,0 +1,44 @@
+package _11722;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class _11722 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int size = Integer.parseInt(br.readLine());
+		int[] arr = new int[size];
+		int[] dp = new int[size];
+		dp[0] = 1;
+		int max = 1;
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < size; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for (int i = 1; i < size; i++) {
+			dp[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (arr[j] > arr[i] && dp[j] + 1 > dp[i]) {
+					dp[i] = dp[j] + 1;
+				}
+			}
+			max = Math.max(max, dp[i]);
+		}
+		
+		bw.write(max + "");
+		
+		bw.flush();
+		bw.close();
+		
+	}
+
+}
