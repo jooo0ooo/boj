2f9612455dcde54887454353bf502b8d06dc4b29
Jiwoo Jung <pingrae@me.com>
2021-01-08 16:49:40 +0900

    boj 15666

---
+++ _15666/_15666.java
@@ -0,0 +1,64 @@
+package _15666;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class _15666 {
+	
+	public static int n, m;
+	public static boolean visited[];
+	public static int[] arr;
+	public static int[] print;
+	public static LinkedHashSet<String> hashSet = new LinkedHashSet<>();
+	public static BufferedWriter bw;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		visited = new boolean[n+1];
+		arr = new int[n+1];
+		print = new int[n+1];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= n; i++) {
+			print[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(print);
+		
+		dfs(1, 1);
+		
+		for (String result : hashSet) {
+			bw.write(result + "\n");
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void dfs (int depth, int count) throws IOException {
+		if (depth == m + 1) {
+			String data = "";
+			for (int i = 1; i <= m; i++) {
+				data += print[arr[i]] + " ";
+			}
+			hashSet.add(data);
+			return;
+		}
+		
+		for (int i = count; i <= n; i++) {
+			arr[depth] = i;
+			dfs(depth + 1, i);
+		}
+		
+	}
+	
+}
